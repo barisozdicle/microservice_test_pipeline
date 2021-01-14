fb1b1702dd8199585dd7a3820f2ce2a4313bf9bd
Barış <bo88@hotmail.co.uk>
2021-01-14 23:50:33 +0300

    add id column to schema

--- testEnv/mysql/schema.sql
+++ testEnv/mysql/schema.sql
@@ -2,10 +2,11 @@ use StarWarsDB;
 
 create table Universe
 (
+    Id                int primary key,
     UniversePeople    varchar(50) not null,
     UniversePlanets   varchar(50) not null,
     UniverseStarships varchar(50) not null
 );
 
-INSERT INTO Universe (UniversePeople, UniversePlanets, UniverseStarships)
-VALUES ('Luke Skywalker', 'Death Star', 'Yavin IV');
+INSERT INTO Universe (Id, UniversePeople, UniversePlanets, UniverseStarships)
+VALUES (1, 'Luke Skywalker', 'Death Star', 'Yavin IV');
